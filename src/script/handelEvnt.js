01eba8a2baab783d71de565cf264992a53522afd
hamed178 <78220953+hamed178@users.noreply.github.com>
2026-08-27 05:12:42 +0300

    Update handelEvnt.js

--- src/script/handelEvnt.js
+++ src/script/handelEvnt.js
@@ -6,69 +6,68 @@ const list = document.querySelector('.country-list');
 class Genral {
     #data = [];
 
-    async Render() {
-        try {
-            list.innerHTML = "";
-            this.#data = [];
+  async Render() {
+    try {
+        list.innerHTML = "";
+        this.#data = [];
 
-            const result = await reqJson(
-                'https://api.restcountries.com/countries/v5?response_fields=names.common,names.official,flag,currencies,population,capitals,region,borders,languages,codes.alpha_2,codes.alpha_3',
-                {
-                    headers: {
-                        Authorization:
-                            'Bearer rc_live_7bf72076d32b4169a7d6606ac99ed8d6'
-                    }
+        renderCard({}, "spiner");
+
+        const result = await reqJson(
+            "https://api.restcountries.com/countries/v5?limit=100&response_fields=names.common,names.official,flag,currencies,population,capitals,region,borders,languages,codes.alpha_2,codes.alpha_3",
+            {
+                headers: {
+                    Authorization: "Bearer rc_live_7bf72076d32b4169a7d6606ac99ed8d6"
                 }
-            );
+            }
+        );
 
-            renderCard({}, 'spiner');
+        const countries = result.data.objects;
 
-            // Convert v5 data to the structure your old project expects
-            const data = result.data.objects.map(country => ({
-                name: {
-                    common: country.names?.common || "",
-                    official: country.names?.official || ""
-                },
+        const data = countries.map(country => ({
+            name: {
+                common: country.names?.common || "",
+                official: country.names?.official || ""
+            },
 
-                flags: {
-                    png: country.flag?.png || "",
-                    svg: country.flag?.svg || "",
-                    alt: country.flag?.alt || ""
-                },
+            flags: {
+                png: country.flag?.png || "",
+                svg: country.flag?.svg || "",
+                alt: country.flag?.alt || ""
+            },
 
-                currencies: country.currencies || {},
+            currencies: country.currencies || {},
 
-                population: country.population || 0,
+            population: country.population || 0,
 
-                capital: country.capitals || [],
+            capital: country.capitals || [],
 
-                region: country.region || "",
+            region: country.region || "",
 
-                borders: country.borders || [],
+            borders: country.borders || [],
 
-                languages: country.languages || {},
+            languages: country.languages || {},
 
-                cca2: country.codes?.alpha_2 || "",
+            cca2: country.codes?.alpha_2 || "",
 
-                cca3: country.codes?.alpha_3 || ""
-            }));
+            cca3: country.codes?.alpha_3 || ""
+        }));
 
-            this.#data = data;
+        this.#data = data;
 
-            list.innerHTML = "";
+        list.innerHTML = "";
 
-            data.forEach(country => {
-                renderCard(country, 'data');
-            });
+        data.forEach(country => {
+            renderCard(country, "data");
+        });
 
-        } catch (error) {
-            console.error(error);
+    } catch (error) {
+        console.error(error);
 
-            list.innerHTML = "";
-            renderCard(error.message, 'error');
-        }
+        list.innerHTML = "";
+        renderCard(error.message, "error");
     }
-
+}
     Seletion() {
         const option = document.querySelector('#selection');
 
